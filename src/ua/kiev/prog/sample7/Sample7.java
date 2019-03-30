2d89bbf60d55a9d03ac1ffd64bda9ddc9948cc67
AlSidorenko <alek.sidorenko1979@gmail.com>
2019-03-30 12:40:36 +0200

    Multithreading. Sample_7.

---
+++ src/ua/kiev/prog/sample7/Sample7.java
@@ -0,0 +1,11 @@
+package ua.kiev.prog.sample7;
+
+/**
+ * Created on 30.03.2019.
+ *
+ * @author Aleks Sidorenko (alek.sidorenko1979@gmail.com).
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class Sample7 {
+}
